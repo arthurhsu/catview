7d6480511ae88bb3169a44759833edb4a0c66ef3
Arthur Hsu <arthurhsu@chromium.org>
2016-05-18 11:53:22 -0700

    Update

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ function convertDate(value) {
 
 (function() {
 
-var dbCrash;
-var dbMain;
-var tableCrash;
-var tableMain;
+var db;
+var table;
+var sections = [];
 
 function createDB() {
-  var builderMain = lf.schema.create('main', 1);
-  builderMain.createTable('line')
+  var builder = lf.schema.create('log', 1);
+  builder.createTable('line')
+      .addColumn('section', lf.Type.STRING)
       .addColumn('no', lf.Type.INTEGER)
       .addColumn('timestamp', lf.Type.INTEGER)
       .addColumn('pid', lf.Type.INTEGER)
@@ -19,33 +19,14 @@ function createDB() {
       .addColumn('type', lf.Type.STRING)
       .addColumn('tag', lf.Type.STRING)
       .addColumn('message', lf.Type.STRING)
-      .addPrimaryKey(['no'])
+      .addPrimaryKey(['section', 'no'])
       .addIndex('idxPid', ['pid'])
       .addIndex('idxTag', ['tag']);
-  builderMain.connect({storeType: lf.schema.DataStoreType.MEMORY}).then(
-      function(db) {
-        dbMain = db;
-        tableMain = db.getSchema().table('line');
-        console.log('main db created');
-      });
-
-  var builderCrash = lf.schema.create('crash', 1);
-  builderCrash.createTable('line')
-      .addColumn('no', lf.Type.INTEGER)
-      .addColumn('timestamp', lf.Type.INTEGER)
-      .addColumn('pid', lf.Type.INTEGER)
-      .addColumn('tid', lf.Type.INTEGER)
-      .addColumn('type', lf.Type.STRING)
-      .addColumn('tag', lf.Type.STRING)
-      .addColumn('message', lf.Type.STRING)
-      .addPrimaryKey(['no'])
-      .addIndex('idxPid', ['pid'])
-      .addIndex('idxTag', ['tag']);
-  builderCrash.connect({storeType: lf.schema.DataStoreType.MEMORY}).then(
-      function(db) {
-        dbCrash = db;
-        tableCrash = db.getSchema().table('line');
-        console.log('crash db created');
+  builder.connect({storeType: lf.schema.DataStoreType.MEMORY}).then(
+      function(instance) {
+        db = instance;
+        table = db.getSchema().table('line');
+        console.log('db created');
       });
 
   // TODO(arthurhsu): make this more robust by waiting for DB to be actually
@@ -74,10 +55,9 @@ function onFileSelected(evt) {
 }
 
 function updateTable() {
-  var db = dbMain;
-  var table = tableMain;
+  var section = $('#section').val();
   var query = db.select().from(table).orderBy(table.no);
-  var searchCondition = [];
+  var searchCondition = [table.section.eq(section)];
   var pid = $('#pid').val().trim();
   if (pid.length) {
     var pids = pid.split(',').map(function(v) {
@@ -90,18 +70,14 @@ function updateTable() {
   var module = $('#tag').val().trim();
   if (module.length) {
     var modules = module.split(',');
-    if (modules.length == 1) {
-      searchCondition.push(table.tag.eq(module));
-    } else {
+    if (modules.length) {
       searchCondition.push(table.tag.in(modules));
     }
   }
   var type = $('#type').val().trim();
   if (type.length) {
     var types = type.split(',');
-    if (types.length == 1) {
-      searchCondition.push(table.type.eq(type));
-    } else {
+    if (types.length) {
       searchCondition.push(table.type.in(types));
     }
   }
@@ -120,25 +96,23 @@ function parseLog(buffer) {
   var rawContents = buffer.split('\n');
   var isCrash = false;
   var lineNumber = 0;
-  var rowsCrash = [];
-  var rowsMain = [];
+  var rows = [];
+  var section = 'unknown';
+  var sectionIndicator = '--------- beginning of ';
   rawContents.forEach(function(line) {
     ++lineNumber;
     if (line.length < 18) return;
-    if (line.trim() == '--------- beginning of crash') {
-      isCrash = true;
-      return;
-    } else if (line.trim() == '--------- beginning of main') {
-      isCrash = false;
+    if (line.indexOf(sectionIndicator) != -1) {
+      section = line.substring(sectionIndicator.length);
+      sections.push(section);
       return;
     }
     var date = Date.parse(line.substring(0, 18));
     var remaining = line.substring(18);
     var sep = remaining.indexOf(':');
     var tokens = remaining.substring(0, sep).replace(/ +(?= )/g,'').trim().split(' ');
-    var table = isCrash ? tableCrash : tableMain;
-    var toPush = isCrash ? rowsCrash : rowsMain;
-    toPush.push(table.createRow({
+    rows.push(table.createRow({
+      'section': section,
       'no': lineNumber,
       'timestamp': date,
       'pid': parseInt(tokens[0] || '-1', 10),
@@ -148,10 +122,13 @@ function parseLog(buffer) {
       'message': remaining.substring(sep + 2)
     }));
   });
-  Promise.all([
-    dbCrash.insert().into(tableCrash).values(rowsCrash).exec(),
-    dbMain.insert().into(tableMain).values(rowsMain).exec()
-  ]).then(updateTable);
+
+  // Update the selector
+  sections.forEach(function(section) {
+    $('#section').append('<option value="' + section + '">' + section + '</option>');
+  });
+  $('#section').val(sections[0]);
+  db.insert().into(table).values(rows).exec().then(updateTable);
 }
 
 // main function
@@ -159,6 +136,7 @@ $(function() {
   $('#load').click(showFile);
   $('#file').change(onFileSelected);
   $('#filter').click(updateTable);
+  $('#section').change(updateTable);
   createDB();
 });
 
