59d9cef0b0b15ee344ed3508760b02756ecfc259
arthurhsu <artchsu@gmail.com>
2016-05-17 23:05:30 -0700

    Slightly better look

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
+var fs = require('fs-extra');
 var gulp = require('gulp');
 var gulpConnect = require('gulp-connect');
 var nopt = require('nopt');
+var path = require('path');
 
 var log = console.log;
+
+var DIST_FILES = [
+  'style.css',
+  'main.js',
+  'node_modules/bootstrap-table/dist/bootstrap-table.css',
+  'node_modules/bootstrap/dist/css/bootstrap.min.css',
+  'node_modules/dateformat/lib/dateformat.js',
+  'node_modules/jquery/dist/jquery.min.js',
+  'node_modules/bootstrap/dist/js/bootstrap.min.js',
+  'node_modules/bootstrap-table/dist/bootstrap-table.min.js',
+  'node_modules/lovefield/dist/lovefield.min.js'
+];
 
 
 function showHelp() {
   log('Usage:');
   log('  gulp server [--port=<port>]');
+  log('  gulp build');
 }
 
 
@@ -16,7 +31,7 @@ gulp.task('default', function() {
 });
 
 
-gulp.task('server', function() {
+gulp.task('server', ['build'], function() {
   var knownOpts = {
     'port': [Number]
   };
@@ -26,8 +41,35 @@ gulp.task('server', function() {
 
   // Start the server
   gulpConnect.server({
-    livereload: false,
+    livereload: true,
     port: port,
-    root: __dirname
+    root: path.join(__dirname, 'build')
   });
+});
+
+gulp.task('build', function() {
+  var buildDir = path.join(__dirname, 'build');
+  fs.ensureDirSync(buildDir);
+
+  // Copy all dependencies flattened.
+  DIST_FILES.forEach(function(fileName) {
+    fs.copySync(fileName, path.join(buildDir, path.basename(fileName)));
+  });
+
+  // Now alter the index.html.
+  var contents =
+      fs.readFileSync(path.join(__dirname, 'index.html'), {encoding: 'utf8'});
+  var newContents =
+      contents.split('\n').map(function(line) {
+        for (var i = 0; i < DIST_FILES.length; ++i) {
+          var file = DIST_FILES[i];
+          if (line.indexOf(file) != -1) {
+            return line.replace(file, path.basename(file));
+          }
+        }
+        return line;
+      }).join('\n');
+  fs.writeFileSync(path.join(buildDir, 'index.html'),
+                   newContents,
+                   {encoding: 'utf8'});
 });
